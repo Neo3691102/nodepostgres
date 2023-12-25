5f51b64a316e816a2c74ad7759837d4d383c865d
Nestor Arturo Castañeda <nestor8772@gmail.com>
2023-12-24 19:51:40 -0600

    Se agrega el módulo pg para poder establecer conexión a base de datos

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,55 @@
+import http from "node:http";
+//import fs from "node:fs";
+import {Pool} from  'pg';
+
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'products',
+    password: 'nestor',
+    port: 5432,
+  });
+
+const server = http.createServer(async (req ,res) => {
+    if(req.method === "GET" && req.url === "/") {
+        //zona de parseo de la consulta SQL
 
 
-console.log("NodeJS + TypeScript");
+        //let Path = "./public/songs.json"; //en lugar de la ruta hay que JSON.stringify el array de la consulta SQL
+        try {
+            const result = await pool.query('SELECT * FROM articles');
+            const data = JSON.stringify(result.rows);
+            res.writeHead(200, {
+              "Content-Type": "application/json",
+              "Content-Length": data.length,
+            });
+            res.end(data);
+          } catch (err) {
+            console.error(err);
+            res.writeHead(500);
+            res.end("Internal Server Error");
+          }
+
+        //termina zona de parseo de la consulta SQL
+
+        //luego de haber parseado, hay que hacer un res.end con el JSON.stringify del array de la consulta SQL
+        //fs.readFile(Path, (err, data) => {
+        //if (err) {
+        //console.error(err);
+        //res.writeHead(500);
+        //res.end("Internal Server Error");
+        //} else {
+        //res.writeHead(200, {
+          //"Content-Type": "application/json",
+          //"Content-Length": data.length,
+        // });
+        //res.end(data);
+      }
+    });
+    //return;
+    //}
+//});  
+
+server.listen(5500, () =>
+  console.log("Servidor ejecutándose en http://localhost:5500/")
+);
